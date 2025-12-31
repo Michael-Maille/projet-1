9adb05e8d822dd0308ee8c57fd8f01ff34178eae
Michael Maille <michaelmaille.dev@gmail.com>
2025-12-31 13:30:58 +0100

    feat: add express api with health and echo routes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-import  http  from "node:http"
+import express from "express";
 
-console.log("le serveur est lancé")
+const app = express();
+app.use(express.json())
+
+app.get("/health", (req, res) => {
+    res.send("le serveur est fonctionnel");
+});
+    
+ 
+app.post("/echo", (req, res) => {
+
+    if (Object.keys(req.body).length === 0){
+        return res.status(400).json({ error: "body vide"});
+    }
+
+    if (!req.body.message) {
+        return res.status(400).json({ error: "champ 'message' manquant"});
+    }
+
+    if (typeof req.body.message !== "string") {
+        return res.status(400).json({ error: "'message' doit être une string"});
+    }
+
+    if (req.body.message.trim()  === "") {
+        return res.status(400).json({ error: "'message' ne doit pas être vide"});
+    }
+
+    res.json({ echo: req.body });
+});
+
+app.listen(3000, () => {
+    console.log("Serveur démarré sur http://localhost:3000")
+});
